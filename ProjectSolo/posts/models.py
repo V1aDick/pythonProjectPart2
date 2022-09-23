d95d8863153fae4c82f9e8d19dd15973aeccadd1
Dm1tryje <muhametshin.mitya@yandex.ru>
2022-09-23 13:32:36 +0500

    k

--- ProjectSolo/posts/models.py
+++ ProjectSolo/posts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Post(models.Model):
     text = models.TextField()
 
-def __str__ (self):
-    return self.text[:50]
+    def __str__ (self):
+        return self.text[:50]
 # Create your models here.
